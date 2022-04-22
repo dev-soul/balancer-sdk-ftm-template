3bac0684c96cf8f3b74ca6c0ce7800c961c5dcc5
dev-soul <87164852+dev-soul@users.noreply.github.com>
2022-04-22 16:41:35 +0600

    initial commit

---
+++ balancer-js/src/constants/contracts.ts
@@ -0,0 +1,5 @@
+export const balancerVaultMap: { [network: number]: string } = {
+    43113: '0x9aed52F3074ba468c6ad17822b8833210868c31b',
+    43114: '0xad68ea482860cd7077a5D0684313dD3a9BC70fbB',
+    250: '0x20dd72Ed959b6147912C2e529F0a0C651c33c9ce',
+};
